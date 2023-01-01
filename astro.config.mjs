c636d24746f371fc9fabea172f6222879528b321
Yuichiro Izumi <yizumi1012xxx@gmail.com>
2023-01-01 23:42:25 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hello-astro-nine.vercel.app",
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), prefetch()],
 });
