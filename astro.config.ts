3f0ec41e89d95f1ea7bcc8fd6cb410bda6aee51a
Yuichiro Izumi <yizumi1012xxx@gmail.com>
2023-01-02 21:58:01 +0000

    use image

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,17 @@ import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://hello-astro-nine.vercel.app",
-  integrations: [react(), sitemap(), prefetch()],
+  integrations: [
+    react(),
+    sitemap(),
+    prefetch(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp"
+    }),
+  ],
 });
