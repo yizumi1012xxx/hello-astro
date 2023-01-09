34ce87d4ca83d5c8bc6a2a712cdc79415cc7804f
Yuichiro Izumi <yizumi1012xxx@gmail.com>
2023-01-09 08:33:07 +0000

    use ssr

--- astro.config.ts
+++ astro.config.ts
@@ -2,19 +2,17 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 import image from "@astrojs/image";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://hello-astro-nine.vercel.app",
   integrations: [
     react(),
     sitemap(),
     prefetch(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp"
-    }),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
   ],
+  adapter: vercel(),
 });
